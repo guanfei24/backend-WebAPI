6a8e05c7084f3e53856e5fc52dacd6c85d4b170c
guanfei24 <78121500+guanfei24@users.noreply.github.com>
2023-04-06 10:38:19 -0400

    update

--- backend-WebAPI/Program.cs
+++ backend-WebAPI/Program.cs
@@ -1,8 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Xml.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,17 +12,27 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapGet("Students", (string? name, int? age) =>
+{
+List<object> stus = new();
+    stus.Add(new
+    {
+        Name = name,
+        Age = age
+    }
+);
+    return Results.Ok(stus);
+});
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+//app.UseAuthorization();
 
-app.UseAuthorization();
-
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
 
